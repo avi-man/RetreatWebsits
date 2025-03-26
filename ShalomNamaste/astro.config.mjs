7cf28b86766abc033121ce04efa0af8529122e88
Your Name <you@example.com>
2025-03-26 22:47:47 +0200

    Add Netlify configuration

--- ShalomNamaste/astro.config.mjs
+++ ShalomNamaste/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: netlify(),
   server: {
     port: 4321,
   },
